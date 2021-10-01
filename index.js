56b543ee040e74cfd74a13aec33b43cc6bc94b42
chiefbiiko <noah.anabiik.schwarz@gmail.com>
2021-10-01 11:06:04 +0200

    fix: dro parsin

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 const { S3, SNS } = require("aws-sdk")
-const {
-  https: { get: _get }
-} = require("follow-redirects")
+const { get } = require("https")
 
-if (!process.env.MEM_BUCKET_NAME) throw Error("env var MEM_BUCKET_NAME unset")
-if (!process.env.TOPIC_ARN) throw Error("env var TOPIC_ARN unset")
+function debug(...args) {
+  console.log("[DEBUG]", ...args)
+}
 
-console.log("process.env.DROPS", process.env.DROPS)
+function parseDrops() {
+  return process.env.DROPS.split(",").map(drop => {
+    const [symbol, perc = "-20%"] = drop.split(/\b(?=-)/)
+    return {
+      symbol: symbol.toLowerCase(),
+      minDropPerc: Number(perc.replace("%", ""))
+    }
+  })
+}
 
-const drops = process.env.DROPS.split(",").map(drop => {
-  const [symbol, perc = "-20%"] = drop.split(/\b(?=-)/)
-  if (!perc.startsWith("-")) throw Error("non-negative price change defined")
-  return {
-    symbol: symbol.toLowerCase(),
-    minDropPerc: Number(perc.replace("%", ""))
-  }
-})
-
-console.log("drops", drops)
+const DROPS_PATTERN = /^[A-Z-_]+(:?-\d+\%)?(:?,[A-Z-_]+(:?-\d+\%)?)*$/
 
 const symbolToCoinGeckoId = Object.freeze({
   eth: "ethereum",
@@ -29,6 +27,16 @@ const symbolToCoinGeckoId = Object.freeze({
   stake: "xdai-stake"
 })
 
+if (!process.env.MEM_BUCKET_NAME) throw Error("env var MEM_BUCKET_NAME unset")
+if (!process.env.TOPIC_ARN) throw Error("env var TOPIC_ARN unset")
+if (!process.env.DROPS) throw Error("env var DROPS unset")
+if (!DROPS_PATTERN.test(process.env.DROPS))
+  throw Error("env var DROPS malformatted, must be CSV, fx 'DOT,MOVR-30%'")
+
+const drops = parseDrops()
+
+debug("drops", drops)
+
 const s3 = new S3({ params: { Bucket: process.env.MEM_BUCKET_NAME } })
 const sns = new SNS()
 
@@ -36,19 +44,19 @@ module.exports.handler = async () => {
   await Promise.all(
     drops.map(async ({ symbol, minDropPerc }) => {
       const coin = await getCoin(symbol)
-      console.log(
+      debug(
         `${symbol} price_change_percentage_24h, minDropPerc`,
         coin.price_change_percentage_24h,
         minDropPerc
       )
-      console.log(
+      debug(
         "typeof coin.price_change_percentage_24h, typeof minDropPerc",
         typeof coin.price_change_percentage_24h,
         typeof minDropPerc
       )
       if (coin.price_change_percentage_24h <= minDropPerc) {
         const alreadyNotified = await memoize(symbol)
-        console.log("alreadyNotified", alreadyNotified)
+        debug("alreadyNotified", alreadyNotified)
         if (!alreadyNotified) {
           const msgTxt = `${symbol.toUpperCase()} dropd ${
             coin.price_change_percentage_24h
@@ -59,7 +67,7 @@ module.exports.handler = async () => {
               TopicArn: process.env.TOPIC_ARN
             })
             .promise()
-          console.log(`just sent msg ${msgId}: "${msgTxt}"`)
+          debug(`just sent msg ${msgId}: "${msgTxt}"`)
         }
       }
     })
@@ -86,9 +94,9 @@ async function memoize(symbol) {
 
 async function getCoin(symbol) {
   return new Promise((resolve, reject) => {
-    _get(
+    get(
       `https://api.coingecko.com/api/v3/coins/${
-        symbolToCoinGeckoId[symbol?.toLowerCase()]
+        symbolToCoinGeckoId[symbol.toLowerCase()]
       }?localization=false`,
       res => {
         const chunks = []
